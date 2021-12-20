52faace83173ca1e7b8599c0c844f0b1fe8f0528
Nekr000 <nekrcode000@gmail.com>
2021-12-20 05:20:48 +0300

    samurai

---
+++ src/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                Hello from Mars
+            </div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Technologies = () => {
+    return (
+        <div>
+            <div>
+                Hello from Earth
+            </div>
+            <ul>
+                <li>Hi</li>
+                <li>QQ</li>
+                <li>RU</li>
+            </ul>
+        </div>
+    )
+}
+export default Technologies;
